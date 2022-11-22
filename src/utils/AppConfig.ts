74df6c1e70e7ad7d06583b47f9aa04ffb96a2b78
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-22 20:32:53 +0700

    add blog template

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+  site_name: 'Code.Is.Mine Blog',
+  title: 'Code.Is.Mine Blog',
+  description: 'Code.Is.Mine provide you a blog that can be improve your knowledge with technology.',
+  author: 'mengsreang-chhoeung',
+  locale_region: 'en-us',
+  locale: 'en',
+};
